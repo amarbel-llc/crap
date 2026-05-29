d81a552b16ef49c940b6f16887d04d65406a50f3
friedenberg <1baggzpr@starbrandshoes.com>
2026-05-29 08:36:22 -0400

    refresh flake.lock + cut nixpkgs input → igloo

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Fork of upstream nixpkgs. overlays.default exposes buildGoApplication,
     # gomod2nix, and other amarbel-llc additions.
-    nixpkgs.url = "github:amarbel-llc/nixpkgs";
+    nixpkgs.url = "github:amarbel-llc/igloo";
     nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
     bats = {
